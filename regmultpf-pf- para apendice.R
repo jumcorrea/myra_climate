06f38e91e94f8f2dd649dcbf42d98fe92cb671ed
Juliana Corrêa <jumartinscorrea@gmail.com>
2023-09-01 14:37:58 -0300

    Update regmultpf-pf- para apendice.R

--- regmultpf-pf- para apendice.R
+++ regmultpf-pf- para apendice.R
@@ -1,36 +1,36 @@
 #carrega o banco de dados do trabalho
-  #pop = população de trabalho
-dados<-read.table("z-pop.txt", h=T, sep="\t")
+  #pop = populaÃ§Ã£o de trabalho
+dados<-read.table("z-pf.txt", h=T, sep="\t")
 
-#lê as primeiras linhas do banco de dados
+#lÃª as primeiras linhas do banco de dados
 head(dados)
 
-#carega o pacote GGally, servirá para gerar os gráficos de pares
+#carega o pacote GGally, servirÃ¡ para gerar os grÃ¡ficos de pares
 library(GGally)
 
-#cria o gráfico de  pares, com dispersão, distribuição e correlação das variáveis do banco de dados
+#cria o grÃ¡fico de  pares, com dispersÃ£o, distribuiÃ§Ã£o e correlaÃ§Ã£o das variÃ¡veis do banco de dados
 #dados[-1] remove a primeira coluna
 ggpairs(dados[-1])
 
-#faz a correlação entre os dados
+#faz a correlaÃ§Ã£o entre os dados
 mc<-cor(dados[-1],use="complete.obs")
 
-#carrega a biblioteca para fazer gráficos coloridos de correlação
+#carrega a biblioteca para fazer grÃ¡ficos coloridos de correlaÃ§Ã£o
 library(corrplot)
 
-#faz o teste de correlação com o pacote corrplot
+#faz o teste de correlaÃ§Ã£o com o pacote corrplot
 mcp<-cor.mtest(mc, conf.level = .95)$p
 
-#desenha o gráfico de correlação colorido
+#desenha o grÃ¡fico de correlaÃ§Ã£o colorido
 corrplot((mc),type="upper",
          method = "ellipse",p.mat = mcp, insig = "label_sig", pch.col="black",pch.cex=.7,
          sig.level = c(.001,.01,.05))
 
-#carrega o pacote para avaliação da colinearidade dos dados
+#carrega o pacote para avaliaÃ§Ã£o da colinearidade dos dados
 library(mctest)
 
-#cria o modelo para testar a colinearidade dos dados de altura/dados climáticos
-  #vs = variável silvicultural; vc = variável climática
+#cria o modelo para testar a colinearidade dos dados de altura/dados climÃ¡ticos
+  #vs = variÃ¡vel silvicultural; vc = variÃ¡vel climÃ¡tica
 c_vs<-lm(dados$vs~vc1+vc2+vc3, data=dados)
 
 #detecta colinearidade geral
@@ -39,12 +39,12 @@ omcdiag(c_vs)
 #detecta colinearidade individual, para cada regressor na matriz X
 imcdiag(c_vs, all=TRUE)
 
-#ver quais são as variáveis a serem removidas, 
-  #pois precisa só das variáveis com médias, não variáveis acumuladas, 
-  #e nem outras variáveis silviculturais
+#ver quais sÃ£o as variÃ¡veis a serem removidas, 
+  #pois precisa sÃ³ das variÃ¡veis com mÃ©dias, nÃ£o variÃ¡veis acumuladas, 
+  #e nem outras variÃ¡veis silviculturais
 names(dados[c(vc1,vc3,vc6,vc8,vc9)])
 
-#remove as variáveis desnecessárias
+#remove as variÃ¡veis desnecessÃ¡rias
 da_vs<-dados[-c(vc1,vc3,vc6,vc8,vc9)]
 
 #cria o modelo
@@ -56,28 +56,28 @@ summary.lm(mod_vs)
 #stepwise: escolhe o modelo de acordo com o menor AIC
 step(mod_vs,direction="backward")
 
-#cria o modelo em função da escolha pelo AIC
+#cria o modelo em funÃ§Ã£o da escolha pelo AIC
 mod_vs<-lm(vs~vc3,data=da_vs)
 
 #resume os dados do modelo linear escolhido
 summary.lm(mod_vs)
 
-#predição do modelo escolhido
+#prediÃ§Ã£o do modelo escolhido
 rs_vs<-predict(mod_vs)
 
-#desenha o histograma da predição do modelo escolhido
+#desenha o histograma da prediÃ§Ã£o do modelo escolhido
 ggplot(data.frame(rs_vs),aes(x=rs_vs))+
   geom_histogram(aes(y=..density..),color="black",fill="gray",binwidth = .3)+
   geom_density(alpha=.2, fill="#FF6666")+
   ggtitle("Histograma")
 
-#desenha o gráfico qq (de resíduos)
+#desenha o grÃ¡fico qq (de resÃ­duos)
 ggplot(data.frame(rs_vs),aes(sample=rs_vs))+ 
   stat_qq() + 
-  stat_qq_line()+labs(x="teórico", y="amostra")+
+  stat_qq_line()+labs(x="teÃ³rico", y="amostra")+
   ggtitle("QQ-Plot")
 
-#desenha o boxplot dos resíduos
+#desenha o boxplot dos resÃ­duos
 ggplot(data.frame(rs_vs),aes(y=rs_vs))+
   geom_boxplot(fill="gray")+
   coord_flip()+
